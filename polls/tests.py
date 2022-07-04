06fc017421e2dd9e77d6cde957b76977870fc07a
abdou_hl <abderrahmane.36.hellal@gmail.com>
2022-07-04 19:35:35 +0100

    add

--- polls/tests.py
+++ polls/tests.py
@@ -18,5 +18,13 @@ class QuestionModelTests(TestCase):
         time = timezone.now() + datetime.timedelta(days=-30)
         future_question = Question(pub_date=time)
         self.assertIs(future_question.was_published_recently(),False)
+    def test_was_published_recently_with_recent_question(self):
+        """
+        return false if the question pub_date in  within the last day
+        """
+        time = timezone.now() + datetime.timedelta(hours=-23)
+        future_question = Question(pub_date=time)
+        self.assertIs(future_question.was_published_recently(),True)
+
 
     
