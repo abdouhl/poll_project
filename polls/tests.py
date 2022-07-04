c4cdf86a1bcc344e90ae8497553511bf26c08ecf
abdou_hl <abderrahmane.36.hellal@gmail.com>
2022-07-04 19:33:56 +0100

    add

--- polls/tests.py
+++ polls/tests.py
@@ -11,3 +11,12 @@ class QuestionModelTests(TestCase):
         time = timezone.now() + datetime.timedelta(days=30)
         future_question = Question(pub_date=time)
         self.assertIs(future_question.was_published_recently(),False)
+    def test_was_published_recently_with_old_question(self):
+        """
+        return false if the question pub_date in  the older then one day
+        """
+        time = timezone.now() + datetime.timedelta(days=-30)
+        future_question = Question(pub_date=time)
+        self.assertIs(future_question.was_published_recently(),False)
+
+    
